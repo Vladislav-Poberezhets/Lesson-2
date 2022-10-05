b184c877fc7c348f6db1115ed187f419868528ca
Vladislav-Poberezhets <vladpoberezhets1@gmail.com>
2022-10-06 02:47:33 +0300

    Добавив 1 и 2 завдання.

---
+++ Lesson 2/Task 1.cs
@@ -0,0 +1,20 @@
+﻿byte x = 2;
+int y = 10;
+
+double a = -23.5;
+
+
+int result1 = -6 * x^3 + 5 * x^2 - 10 * x + 15;
+
+Console.WriteLine(result1);
+
+double result2 = Math.Abs(a) * Math.Sin(y);
+
+Console.WriteLine(result2);
+
+double result3 = 2 * Math.PI * y;
+
+Console.WriteLine(result3);
+
+
+Console.WriteLine(Math.Max(x, y)); 
---
+++ Task 2/Task2.cs
@@ -0,0 +1,16 @@
+﻿var now = DateTime.Today;
+
+
+DateTime newYear2023 = new DateTime(2023, 01, 01 ) ;
+
+DateTime newYear2022 = new (2022, 01, 01 );
+
+Console.WriteLine(now - newYear2022 + "days passed from New Year");
+
+Console.WriteLine(newYear2023 - now + "days left to New Year");
+
+Console.ReadKey();
+
+
+
+
